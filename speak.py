c3f6d37652fe17b3950d91b1c75c886e4e956b23
gray-guy <mshehroz99@hotmail.com>
2021-03-07 16:19:03 +0300

    Play sounds on a seperate thread

--- speak.py
+++ speak.py
@@ -1,16 +1,20 @@
 import boto3
 from playsound import playsound
+from threading import Thread
 import os
 
 polly_client = boto3.Session(profile_name='default', region_name='us-east-1').client('polly')
 
-def speakText(inputText):
+def play_music():
+    playsound('speech.mp3')
+    os.remove('speech.mp3')
 
+def speakText(inputText):
     response = polly_client.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3', Text = inputText)
 
     file = open('speech.mp3', 'wb')
     file.write(response['AudioStream'].read())
     file.close()
 
-    playsound('speech.mp3')
-    os.remove('speech.mp3')
+    music_thread = Thread(target=play_music)
+    music_thread.start()
